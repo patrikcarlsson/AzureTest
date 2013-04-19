feed4fe99b091392959fe16423fc73b1ae672678
Patrik Carlsson <patrik.carlsson@ayond.se>
2013-04-19 10:05:26 +0200

    Import.

---
+++ AzureTestWeb/AzureTestWeb/Controllers/HomeController.vb
@@ -0,0 +1,21 @@
+﻿Public Class HomeController
+    Inherits System.Web.Mvc.Controller
+
+    Function Index() As ActionResult
+        ViewData("Message") = "Patrik tests Azure"
+
+        Return View()
+    End Function
+
+    Function About() As ActionResult
+        ViewData("Message") = "Azure test"
+
+        Return View()
+    End Function
+
+    Function Contact() As ActionResult
+        ViewData("Message") = "Patrik Carlsson"
+
+        Return View()
+    End Function
+End Class
