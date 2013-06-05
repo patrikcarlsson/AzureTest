1934f06d08714dcf14a89ca800d98e0c2b82cc8a
Patrik Carlsson <patrik.carlsson@ayond.se>
2013-06-05 07:20:03 +0200

    Update in test branch.

--- AzureTestWeb/AzureTestWeb/Controllers/HomeController.vb
+++ AzureTestWeb/AzureTestWeb/Controllers/HomeController.vb
@@ -2,7 +2,7 @@
     Inherits System.Web.Mvc.Controller
 
     Function Index() As ActionResult
-        ViewData("Message") = "Patrik tests Azure"
+        ViewData("Message") = "Patrik tests Azure in a branch (not master)"
 
         Return View()
     End Function
